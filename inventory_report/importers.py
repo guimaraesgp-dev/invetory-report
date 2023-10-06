f6ffd60666c139d9f20beef5150d6b241efdf2ab
Gabriel Guimarães <gabrielguimaraes.dev@gmail.com>
2023-10-06 02:52:45 -0300

    REQ 4

--- inventory_report/importers.py
+++ inventory_report/importers.py
@@ -1,5 +1,7 @@
 from typing import Dict, Type, List
 from abc import ABC, abstractmethod
+import json
+from inventory_report.product import Product
 
 
 class Importer(ABC):
@@ -7,12 +9,15 @@ class Importer(ABC):
         self.path = path
 
     @abstractmethod
-    def import_data(self) -> List[str]:
+    def import_data(self) -> List[Product]:
         pass
 
 
-class JsonImporter:
-    pass
+class JsonImporter(Importer):
+    def import_data(self, path: str) -> List[Product]:
+        with open(path, "r") as file:
+            product_data = json.load(file)
+            return [Product(**product) for product in product_data]
 
 
 class CsvImporter:
